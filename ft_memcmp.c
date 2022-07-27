2acceb289f57f8fb245421aefd530d126bfe5fe5
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-27 13:53:25 +0100

    The  memcmp()  function compares the first n bytes (each interpreted as        unsigned char) of the memory areas s1 and s2.

---
+++ ft_memcmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                              *       ::##::  #  ::#####    */
+/*   ft_memcmp.c                                *      ::    :: . ::          */
+/*                                              *      ::    :: . ::          */
+/*   By: ael-bako <ael-bako@outlook.com>        *      ::####:: # ::####      */
+/*                                              *      :#    #: . ::          */
+/*   Created: 2022/07/27 13:38:40 by ael-b      *      #:    :# . ::          */
+/*   Updated: 2022/07/27 13:38:43 by ael-b      *      :#    #: #  ::#####    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+			str1++;
+			str2++;
+	}
+	return (0);
+}
+// int main()
+// {
+// char csrc[] = "GeeksforGeeks";
+// char cdest[] = "5656";
+
+//     printf("%d ", ft_memcmp(cdest, csrc, strlen(csrc) - 1));
+// return 0;
+// }
