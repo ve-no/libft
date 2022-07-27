655f9f8a23db4ba48bb89c2291e46386451ed6c9
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-27 21:42:27 +0100

    its like strncat but diffrence strlcat more secure like strncpy with strlcpy

---
+++ ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                              *       ::##::  #  ::#####    */
+/*   ft_strlcat.c                               *      ::    :: . ::          */
+/*                                              *      ::    :: . ::          */
+/*   By: ael-bako <ael-bako@outlook.com>        *      ::####:: # ::####      */
+/*                                              *      :#    #: . ::          */
+/*   Created: 2022/07/27 21:13:48 by ael-b      *      #:    :# . ::          */
+/*   Updated: 2022/07/27 21:13:52 by ael-b      *      :#    #: #  ::#####    */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	res;
+	size_t	i;
+
+	i = 0;
+	dest_len = strlen(dest);
+	src_len = strlen(src);
+	if (size < dest_len)
+		res = dest_len + src_len;
+	else
+		res = src_len + size;
+	while (src[i] && (dest_len + 1) < size)
+		dest[dest_len++] = src[i++];
+	dest[dest_len] = 0;
+	return (res);
+}
+// int	main(void)
+// {
+// 	char	dest[] = "hello dest";
+// 	char	src[]= "world ";
+
+// 	printf("%ld \n", ft_strlcat(dest, src, 12));
+// 	printf("%s\n", dest);
+// }
