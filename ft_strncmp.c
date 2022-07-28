36bc673cea89df8e346e277963a6a39ffa6c36a1
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-28 16:11:45 +0100

    The strncmp() function is similar, except it compares only the first (at most) n bytes of s1 and s2.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,8 +18,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((unsigned char)s1[i] == (unsigned char)
-		&&s2[i] && s1[i] && s2[i] && i < n - 1)
+	while ((unsigned char)s1[i] == (unsigned char)s2[i]
+		&& s2[i] && s1[i] && s2[i] && i < n - 1)
 		i++;
 	if (n > 0)
 		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
