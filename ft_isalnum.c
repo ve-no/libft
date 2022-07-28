614f6e60954bdaf4e1a5b5faa4fc32726e91c6ae
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-07-28 16:05:15 +0100

    checks for an alphanumeric character; it is equivalent to (isalpha(c) || isdigit(c))

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,7 +12,8 @@
 
 int	ft_isalnum(int c)
 {
-	if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A' || c <= '9' && c >= '0')
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')
+		|| (c <= '9' && c >= '0'))
 		return (1);
 	return (0);
 }
