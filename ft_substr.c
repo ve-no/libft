d8e1fde7de4856093edee4bff35766db73374e16
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-01 20:25:32 +0100

    The substr() function returns the substring of a given string between two given indices. The substr function prototype is: char* substr(const char *source, int m, int n) It returns the substring of the source string starting at the position m and ending at position n-1 .

---
+++ ft_substr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                              *       ::##::  #  ::#####    */
+/*   ft_substr.c                                *      ::    :: . ::          */
+/*                                              *      ::    :: . ::          */
+/*   By: ael-bako <ael-bako@outlook.com>        *      ::####:: # ::####      */
+/*                                              *      :#    #: . ::          */
+/*   Created: 2022/08/01 19:52:51 by ael-b      *      #:    :# . ::          */
+/*   Updated: 2022/08/01 19:52:53 by ael-b      *      :#    #: #  ::#####    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	int		j;
+
+	j = 0;
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	len = start + (unsigned int)len;
+	while (start < (unsigned int)len && (char)s[start] != '\0')
+		str[j++] = (char)s[start++];
+	str[j] = '\0';
+	return (str);
+}
+int main()
+{
+	char src[] = "substr function Implementation";
+
+	int m = 9;
+	int n = 10;
+
+	char* dest = ft_substr(src, m, n);
+
+	printf("%s\n", dest);
+
+	return 0;
+}
