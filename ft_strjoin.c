e81fd92be5de814898419ce4569fa30451fc3523
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-03 17:46:29 +0100

    The strjoin functon creates a string that consists of str1 concatenated with str2. The returned string is allocated out of scratch memory and is therefore valid only for the duration of the clause. If insufficient scratch memory is available, strjoin does not execute and an error is generated.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,22 +20,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		k;
 
-	k = strlen(s1) + strlen(s2) +1 ;
+	k = ft_strlen(s1) + ft_strlen(s2) +1 ;
 	str = (char *)malloc(k);
 	if (!str)
 		return (NULL);
 	str = strdup(s1);
 	i = 0;
 	k = 0;
-	while (i < (int)strlen(s1))
+	while (i < (int)ft_strlen(s1))
 		str[k++] = s1[i++];
 	i = 0;
-	while (i < (int)strlen(s2))
+	while (i < (int)ft_strlen(s2))
 		str[k++] = s2[i++];
 	str[k] = '\0';
 	return (str);
 }
-// int main()
-// {
-// 	printf(":%s:\n", ft_strjoin("helo, world!", "ksjghks"));
-// }
+int main()
+{
+	printf(":%s:\n", ft_strjoin("helo, world!", "ksjghks"));
+}
