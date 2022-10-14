e107b4827152c90ced296c323b80c9597add6814
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-10-14 17:01:32 +0100

    just a commit

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ael-bako <ael-bako@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:35:29 by ael-bako          #+#    #+#             */
-/*   Updated: 2022/10/11 13:04:37 by ael-bako         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:00:31 by ael-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,21 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		f(i, &s[i]);
 		i++;
 	}
+}
+
+void	fun(unsigned int i, char *s)
+{
+	*s -= 32;
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char *str;
+	int *p;
+
+	char	s[] = "elbakouri";
+	ft_striteri(s, fun);
+	printf("%s", s);
 }
